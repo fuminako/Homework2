2ed03f47bcc7f0795b2b575dc94d1008bfedf314
Анастасия <fumi_nako@mail.ru>
2022-09-07 00:14:04 +0300

    Домашнее 2 Я смотрела видео чтобы проверить по нему код, из-за этого несколько раз не зачло :(

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задача 2");
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задача 3");
+        var dogTwo = 8.0;
+        dogTwo = dogTwo - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dogTwo);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        System.out.println("Задача 6");
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        var totalWeight = firstBoxer + secondBoxer;
+        System.out.println("Общий вес " + totalWeight + " кг");
+
+        System.out.println("Задача 7");
+        var weightDifference = secondBoxer - firstBoxer;
+        System.out.println("Разница в весе (1) " + weightDifference + " кг");
+        var weightDifferenceTwo = secondBoxer % firstBoxer;
+        System.out.println("Разница в весе (2) " + weightDifferenceTwo + " кг");
+
+        System.out.println("Задача 8");
+        var hours = 640;
+        var workerWorking = 8;
+        var employee = hours / workerWorking;
+
+        System.out.println("Всего работников в компании " + employee + " человек");
+
+        var allWorkers = employee + 94;
+        var allHours = allWorkers * workerWorking;
+        System.out.println("Если в компании работает " + allWorkers + " человека, то всего " + allHours + " часа работы может быть поделено между сотрудниками");
+    }
+}
